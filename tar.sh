dd6de23ffe227b8cc7568565aa847914b82af0bc
xiaohutu <shanxi.1991@gmail.com>
2021-05-04 18:37:52 +0800

    打包脚本 zip压缩 增加-q参数

--- tar.sh
+++ tar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf easy-git.zip
-zip -r easy-git.zip . -x /.git/* /src/common/mix.source.js /src/common/oauth.source.js /run.sh /tar.sh /.gitignore /.gitattributes
+zip -q -r easy-git.zip . -x /.git/* /src/common/mix.source.js /src/common/oauth.source.js /run.sh /tar.sh /.gitignore /.gitattributes
