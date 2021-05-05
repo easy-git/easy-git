2f75f5c3a086ada3d5c888becc8d0b838f6a7497
xiaohutu <shanxi.1991@gmail.com>
2021-05-05 22:24:20 +0800

    优化 git授权认证 正确的打印错误消息

--- src/common/axios.js
+++ src/common/axios.js
@@ -3,28 +3,36 @@ const axios = require('axios');
 /**
  * @description axios post请求
  */
-function axiosPost(url, params={}, customHeader={}) {
-    let headers = {"Accept": "application/json", "content-type": "application/json"};
+function axiosPost(url, params = {}, customHeader = {}) {
+    let headers = {
+        "Accept": "application/json",
+        "content-type": "application/json"
+    };
     if (JSON.stringify(customHeader) != "{}") {
         headers = Object.assign(headers, customHeader);
     };
     const instance = axios.create({
-        timeout: 30000,
+        timeout: 1000,
         headers: headers
     });
     return new Promise((resolve, reject) => {
-        axios.post(url,params).then((res) => {
-            resolve(res.data)
+        instance.post(url, params).then((res) => {
+            resolve(res.data);
         }).catch((err) => {
             if (err.response) {
                 console.error("[easy-git] axios request ::", err.response.data, err.response.status);
                 reject(err.response.data);
             } else {
-                console.error("[easy-git] axios request ::", err);
-                reject("fail");
+                if (err.code == 'ECONNABORTED') {
+                    reject("ETIMEDOUT");
+                } else {
+                    reject("fail");
+                };
             };
         });
-    })
+    }).catch((error) => {
+        return error;
+    });
 };
 
 /**
@@ -32,14 +40,18 @@ function axiosPost(url, params={}, customHeader={}) {
  * @param {Object} url
  */
 function axiosGet(url) {
-    const instance = axios.create({timeout: 5000});
+    const instance = axios.create({
+        timeout: 5000
+    });
     return new Promise((resolve, reject) => {
         instance.get(url).then((res) => {
-            resolve(res.data)
+            resolve(res.data);
         }).catch((err) => {
-            reject("fail")
+            reject("fail");
         });
-    })
+    }).catch((error) => {
+        return error;
+    });
 };
 
 
