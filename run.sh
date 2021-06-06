a598a8fc6ae3dacec421a8523a1664d07658cec7
xiaohutu <shanxi.1991@gmail.com>
2021-06-06 16:31:38 +0800

    优化 shell测试脚本

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 hxVersion="HBuilderX.app"
 
-if [ $1=="release" ];then
+if [ "$1" == "release" ];then
     hxVersion='HBuilderX.app'
 fi
 
-if [ $1=="alpha" ];then
+if [ "$1" == "alpha" ];then
     hxVersion='HBuilderX-Alpha.app'
 fi
 
@@ -21,7 +21,7 @@ ps -ef | grep $hxVersion | grep -v grep | awk '{print $3}' | xargs kill -9
 cd $easyGitSourceDir && rm -rf easy-git.zip
 zip -q -r easy-git.zip . -x /.git/* /src/common/mix.source.js /src/common/oauth.source.js /run.sh /tar.sh /.editorconfig /.gitignore /.gitattributes
 cp easy-git.zip $plugin_dir
-cd $plugin_dir && rm -rf easy-git && unzip easy-git.zip -d easy-git
+cd $plugin_dir && rm -rf easy-git && unzip -q easy-git.zip -d easy-git
 
 # 重新启动
 $cli_dir"cli" open
