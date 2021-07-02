832262d1725d1733a6d6b92a1213afac8e7d07d3
xiaohutu <shanxi.1991@gmail.com>
2021-07-02 22:00:51 +0800

    优化 克隆视图 增加版本限制，要求3.1.2

--- src/view/clone/clone.js
+++ src/view/clone/clone.js
@@ -1,11 +1,9 @@
 const hx = require('hbuilderx');
 
-// hbuilderx 3.1.2-, use webview
-const openWebView = require('./view_webview.js');
-
 // hbuilderx 3.1.2+, use webviewDialog
 let openWebDialog = require('./view_webdialog.js');
 
+const {hxShowMessageBox} = require('../../common/utils.js');
 const cmp_hx_version = require('../../common/cmp.js');
 
 // get hbuilderx version
@@ -18,11 +16,22 @@ function openCloneView(webviewPanel=undefined) {
         if (cmp <= 0) {
             openWebDialog();
         } else {
-            openWebView(webviewPanel);
+            hxShowMessageBox(
+                "EasyGit",
+                "Git克隆操作，在HBuilderX 3.1.2+版本，优化界面UI以及相关功能。\n\n建议升级HBuilderX到3.1.2+版本。",
+                 ["我知道了"]
+            );
         }
     }catch(e){
-        console.log(e)
-        openWebView(webviewPanel);
+        hxShowMessageBox(
+            "EasyGit",
+            "Git克隆操作界面，打开异常，请联系开发者。或升级HBuilderX到最新版本。",
+             ["寻求帮助","我知道了"]
+        ).then( btn => {
+            if (btn == '寻求帮助') {
+                hx.env.openExternal('https://ext.dcloud.net.cn/plugin?id=2475');
+            }
+        })
     };
 };
 
