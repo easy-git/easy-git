2a5edfc2572494a271a47f67af72672833b04515
xiaohutu <shanxi.1991@gmail.com>
2021-12-24 21:06:00 +0800

    优化 快速打开项目 支持打开本地磁盘上的项目

--- src/commands/quick_open.js
+++ src/commands/quick_open.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 const {
-    getFilesExplorerProjectInfo
+    getFilesExplorerProjectInfo,
+    checkIsGitProject
 } = require('../common/utils.js');
 const count = require('../common/count.js');
+
 
 /**
  * @description 快速打开项目管理器的Git项目、或本地的Git项目
@@ -19,13 +21,15 @@ async function quickOpen(param) {
     if (success && Folders.length) {
         for (let s of Folders) {
             if (s.isGit) {
-                gitList.push({"label": s.FolderName, "description": s.GitRepository, "projectPath":s.GitRepository });
+                gitList.push({"label": s.FolderName, "description": s.GitRepository, "projectPath":s.GitRepository, "action": "open"});
             };
         };
     };
 
     let pickerList = [
-         ...gitList
+        {"label": "没有找到? 克隆仓库", "action": "clone"},
+        {"label": "没有找到? 打开本地磁盘上的Git项目", "action": "openLocal"},
+        ...gitList
     ];
     let selected = await hx.window.showQuickPick(pickerList, {
         'placeHolder': '请选择要打开的项目...'
@@ -34,14 +38,104 @@ async function quickOpen(param) {
     });
 
     if (selected) {
-        let {projectPath, label} = selected;
+        let {projectPath, label, action} = selected;
         let info = {
             "projectPath": projectPath,
             "projectName": label,
             "easyGitInner": true
         };
-        hx.commands.executeCommand("EasyGit.main", info);
+
+        if (action == 'clone') {
+            hx.commands.executeCommand("EasyGit.clone");
+        };
+        if (action == 'openLocal') {
+            selecteOpenLocalProject();
+        };
+        if (action == 'open') {
+            hx.commands.executeCommand("EasyGit.main", info);
+        };
+
     };
+};
+
+
+async function goValidate(formData, that) {
+    // 检查：所有项不能为空
+    for (let v in formData) {
+        let info = (formData[v]).trim();
+        if (info == "" || !info) {
+            that.showError(`不能为空或填写错误`);
+            return false;
+        };
+    };
+
+    let {GitDir} = formData;
+
+    let isExists = fs.existsSync(GitDir);
+    if (!isExists) {
+        that.showError(`选择的路径不存在！`);
+        return false;
+    };
+    let state = fs.statSync(GitDir);
+    if (!state.isDirectory()) {
+        that.showError(`请选择正确的目录`);
+        return false;
+    };
+
+    if (GitDir) {
+        let isCheck = await checkIsGitProject(GitDir).catch( error => {
+            return 'No'
+        });
+        if (isCheck != 'No') {
+            return true;
+        } else {
+            that.showError(`当前选择的目录不是Git项目！`);
+        };
+    };
+    return false;
+};
+
+/**
+ * @description 选择并打开本地磁盘上的项目
+ */
+async function selecteOpenLocalProject() {
+    let subtitle = '请选择本地磁盘上的Git项目';
+    let formItems = [
+        {type: "fileSelectInput",name: "GitDir",label: "选择",placeholder: '请选择本地磁盘上的Git项目', mode: "folder"},
+    ];
+
+    let Selected = await hx.window.showFormDialog({
+        formItems: formItems,
+        title: "打开Git项目",
+        subtitle: subtitle,
+        width: 480,
+        height: 240,
+        submitButtonText: "确定(&S)",
+        cancelButtonText: "取消(&C)",
+        validate: function(formData) {
+            this.showError('');
+            let checkResult = goValidate(formData, this);
+            return checkResult;
+        }
+    }).then((res) => {
+        return res;
+    }).catch(error => {
+        console.log(error);
+    });
+
+    if (!Selected) return;
+    try{
+        let {GitDir} = Selected;
+        let projectName = path.basename(GitDir);
+        let info = {
+            "projectPath": GitDir,
+            "projectName": projectName,
+            "easyGitInner": true
+        };
+        hx.commands.executeCommand("EasyGit.main", info);
+    }catch(e){
+        hx.window.showErrorMessage('EasyGit: 打开项目失败', ['我知道了']);
+    }
 };
 
 module.exports = quickOpen;
