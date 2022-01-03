91a9b94d02e7d41aa7c46f998df166d3627071b3
xiaohutu <shanxi.1991@gmail.com>
2022-01-03 21:47:33 +0800

    调整 blame 支持选择以何种方式呈现

--- src/commands/blame.js
+++ src/commands/blame.js
@@ -1,6 +1,42 @@
 const hx = require('hbuilderx');
 
-const { gitRaw } = require('../common/utils.js');
+const {
+    gitRaw,
+    hxShowMessageBox,
+    createOutputChannel
+ } = require('../common/utils.js');
+
+let config = hx.workspace.getConfiguration();
+
+/**
+ * @description 以何种方式显示blame信息
+ */
+async function getGitBlameDisplayMode() {
+    let blameDisplayMode = config.get('EasyGit.blameDisplayMode');
+    if (blameDisplayMode) {
+        blameDisplayMode = blameDisplayMode.toLowerCase();
+        if (["console", "statusbar", "message"].includes(blameDisplayMode)) {
+            return blameDisplayMode;
+        };
+    };
+    let Confirm = await hxShowMessageBox(
+        "请选择Git Blame信息的展示方式",
+        "此次选择后，后期不再提示。",
+        ['右下角弹窗', "底部状态栏", "控制台"],
+    ).then(btnText => {
+        return btnText;
+    });
+    let mode;
+    if (Confirm == "控制台") {
+        mode = "console";
+    } else if (Confirm == "底部状态栏") {
+        mode = "statusbar";
+    } else {
+        mode = "message";
+    };
+    config.update("EasyGit.blameDisplayMode", mode).then(() => {});
+    return mode;
+};
 
 /**
  * @description 显示文件的每一行最后修改的版本和作者
@@ -27,7 +63,18 @@ async function gitBlameForLineChange(projectPath, selectedFile) {
                 return hx.window.setStatusBarMessage(errorMsg, 5000, 'error');
             };
             m = m.substr(0, m.length - 1);
-            hx.window.setStatusBarMessage(`Git: 当前行最后修改信息:  ${m}`, 20000, 'info');
+
+            let commit = result.split(' ')[0];
+            let last_message = `Git: 当前选择行最后修改信息: \n\ncommit: ${commit}\n最后修改：${m}`;
+
+            let displayMode = await getGitBlameDisplayMode();
+            if (displayMode == 'statusbar') {
+                hx.window.setStatusBarMessage(`Git: 当前选择行最后修改信息:  ${m}`, 20000, 'info');
+            } else if (displayMode == 'message') {
+                hx.window.showInformationMessage(last_message, ["我知道了"]);
+            } else {
+                createOutputChannel(last_message, "info");
+            };
         }catch(e){
             console.log(e)
             return hx.window.setStatusBarMessage(errorMsg, 20000, 'error');
